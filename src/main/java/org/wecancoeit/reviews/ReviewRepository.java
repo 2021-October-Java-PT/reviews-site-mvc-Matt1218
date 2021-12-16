de056f6c250839b8bf872f9233d9818ea5fda146
Matt1218 <welfleym@gmail.com>
2021-12-16 15:08:40 -0500

    Need to add styling and html elements to pages. Also need to change values in reviewRepository

--- src/main/java/org/wecancoeit/reviews/ReviewRepository.java
+++ src/main/java/org/wecancoeit/reviews/ReviewRepository.java
@@ -3,8 +3,37 @@ package org.wecancoeit.reviews;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 @Repository
 public class ReviewRepository {
+
+    Map<Long, Review> reviewsList = new HashMap<>();
+
+    public ReviewRepository(){
+        Review tentOne = new Review(1L, "PlaceholderTitle", "/imageURL", "reviewCategory", "content");
+        Review tentTwo = new Review(2L,"PlaceholderTitle", "/imageURL", "reviewCategory", "content");
+        Review tentThree = new Review(3L,"PlaceholderTitle", "/imageURL", "reviewCategory", "content");
+        Review tentFour = new Review(4L,"PlaceholderTitle", "/imageURL", "reviewCategory", "content");
+        Review tentFive = new Review(5L,"PlaceholderTitle", "/imageURL", "reviewCategory", "content");
+
+        reviewsList.put(tentOne.getId(),tentOne);
+        reviewsList.put(tentTwo.getId(),tentTwo);
+        reviewsList.put(tentThree.getId(),tentThree);
+        reviewsList.put(tentFour.getId(),tentFour);
+        reviewsList.put(tentFive.getId(),tentFive);
+    }
+
+    public Review findOne(long id){
+        return reviewsList.get(id);
+    }
+
+    public Collection<Review> findAll(){
+        return reviewsList.values();
+    }
+
 
 
 }
